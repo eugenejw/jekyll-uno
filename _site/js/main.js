377db6e79ae3e5cec3530799ac497ce0b12f7f8c
Weihan Jiang <weihan.github@gmail.com>
2017-08-22 18:08:29 -0700

    changed layout

--- _site/js/main.js
+++ _site/js/main.js
@@ -23,10 +23,6 @@ $(document).ready(function () {
     }
   })
 
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
